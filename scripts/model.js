c2c5e611fb3aeaee18edd52794eac8e6b487cd2b
mikevane <michalvanek27@gmail.com>
2024-01-19 12:50:57 +0100

    Search functionality implemented

--- scripts/model.js
+++ scripts/model.js
@@ -3,16 +3,16 @@ import { getJSON } from "./helpers.js";
 import "core-js/stable";
 import { async } from "regenerator-runtime";
 
-const screener = document.getElementById("screener");
+let screener = document.getElementById("screener");
 const searchField = document.querySelector(".search__field");
 const searchButton = document.querySelector(".search__btn");
 
-export let stock = {};
+let stock = {};
 
-let pokus = 4;
-
+// Converting data received from API into more usable format
 const createStockReportObject = function (data) {
   console.log(data);
+  // desctructuring "data" object received in loadStock function
   let {
     company_name,
     tickers,
@@ -39,12 +39,15 @@ const createStockReportObject = function (data) {
   };
 };
 
+// Load stock that is requested through search field
 const loadStock = async function (ticker) {
   try {
+    // Call API with a requested ticker
     const data = await getJSON(`${API_URL}${API_KEY}&ticker=${ticker}`);
+    // Convert data using another function
     stock = createStockReportObject(data.results[1]);
     console.log(stock);
-
+    // Populate screener
     fillScreen();
   } catch (err) {
     console.error(
@@ -57,25 +60,48 @@ const loadStock = async function (ticker) {
 // SEARCH FUNCIONALITY
 searchButton.addEventListener("click", function (e) {
   e.preventDefault();
-  // collects the ticker from the field
+  // Collects the ticker from the field
   ticker = searchField.value;
-  // loads the stock using loadStock function
+  // Loads the stock using loadStock function
   loadStock(ticker);
-  // clears the search field for the next usage
+  // Clears the search field for the next usage
   searchField.value = "";
 });
 
+// Renders the data
 const fillScreen = function () {
+  // Formats dates into Czech format
   stock.start = new Date(stock.start);
   stock.end = new Date(stock.end);
   stock.start = new Intl.DateTimeFormat("cs-CZ").format(stock.start);
   stock.end = new Intl.DateTimeFormat("cs-CZ").format(stock.end);
 
+  // Empties old data if there's any
+  screener.innerHTML = "";
+
+  // Creates the HTML
   const basic_info = `      
-  <h3>${stock.company_name} (${stock.tickers})</h3>
-      <p>Report pro <b>${stock.quarter} ${stock.year}</b>
-      <br />(Období od ${stock.start} do ${stock.end})</p>
-      <h4>Rozvaha</h4>
-      <p>Aktiva: ${stock.financials.balance_sheet.assets.unit} ${stock.financials.balance_sheet.assets.value}
-      <br />Pasiva: ${stock.financials.balance_sheet.liabilities.unit} ${stock.financials.balance_sheet.liabilities.value}</p>`;
+    <h3>${stock.company_name} (${stock.tickers})</h3>
+    <p>Report pro <b>${stock.quarter} ${stock.year}</b>
+    <br />(Období od ${stock.start} do ${stock.end})</p>
+    <h4>Rozvaha</h4>
+    <p>Aktiva: ${stock.financials.balance_sheet.assets.unit} ${stock.financials.balance_sheet.assets.value}
+    <br />Pasiva: ${stock.financials.balance_sheet.liabilities.unit} ${stock.financials.balance_sheet.liabilities.value}</p>
+    `;
+
+  // Inserts the HTML into screener div
+  screener.insertAdjacentHTML("afterbegin", basic_info);
 };
+
+// -- TODO --
+
+// CODING
+// 1) Make sure lower-case tickers are converted before executing
+// 2) Fix error handling
+// 3) loading older reports as well
+
+// CONTENT
+// 1) Load more financial data (almost all of them)
+
+// WEBDESING
+// 1) Pretty much everything
